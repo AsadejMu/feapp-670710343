179bdb9e8af4f6403934e3d414f8f755f72bbe58
Asadej Munthawonwong <munthawonwong_a@silpakorn.edu>
2026-09-10 03:43:32 +0000

    ready to deploy

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
-      <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+      <p className="mt-2 text-slate-500">มาเด้อสู MovieHub</p>
     </div>
   );
 }
